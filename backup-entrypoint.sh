a94a6b7574dd945c435d099bdb55801cfdcb1f2c
xingjiudong <25635680@qq.com>
2017-11-06 11:42:05 +0800

    Fix Dockerfile

--- backup-entrypoint.sh
+++ backup-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-: ${HOST:=127.0.0.1}
-: ${DB:=admin}
-: ${USERNAME:=siteRootAdmin}
-: ${PASSWORD:=password}
-: ${AUTHENTICATIONDATABASE:=admin}
-: ${OUT:=/data}
+HOST=${HOST}
+DB=${DB}
+USERNAME=${USERNAME}
+PASSWORD=${PASSWORD}
+AUTHENTICATIONDATABASE=${AUTHENTICATIONDATABASE}
+OUT=${OUT}
 
-mongodump -h ${HOST} -d ${DB} -u ${USERNAME} -p ${PASSWORD} --authenticationDatabase ${AUTHENTICATIONDATABASE} -o  ${OUT} 
+mongodump -h ${HOST} -d ${DB} -u ${USERNAME} -p ${PASSWORD} --authenticationDatabase ${AUTHENTICATIONDATABASE} -o ${OUT} 
 
 source /usr/local/bin/docker-entrypoint.sh
